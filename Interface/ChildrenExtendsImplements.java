4935a33b78d0e9c312f211e40e60359c4306ef4c
MMMMM104 <624244368@qq.com>
2019-04-28 21:55:17 +0800

    Interface

---
+++ Interface/ChildrenExtendsImplements.java
@@ -0,0 +1,57 @@
+/*
+package Interface;
+//一个子类要实现接口有需要继承抽象类时，先继承后实现接口
+
+interface IMessage {
+    public void print(); // 接口中可以省略abstract
+}
+
+abstract class News {
+    public abstract void getNews(); //抽象类中不能省略abstract,否则就是普通方法
+}
+
+class MessageImpl extends News implements IMessage {
+    @Override
+    public void print() {
+        System.out.println("i am a biter1");
+    }
+
+    @Override
+    public void getNews() {
+        System.out.println("i am a biter2");
+    }
+}
+
+public class ChildrenExtendsImplements {
+    public static void main(String[] args) {
+        IMessage I = new MessageImpl();
+        I.print();
+        News n = (News)I;
+        n.getNews();
+    }
+}
+*/
+//抽象类可以使用implements实现多个接口，但接口不能继承抽象类
+package Interface;
+interface A{
+    public void print();
+}
+abstract class B implements A{
+     public  abstract  void getNews();
+}
+class C extends B{
+    public void print(){
+        System.out.println("mjy is pig");
+    }
+    public void getNews(){
+        System.out.println("mjy is pig");
+    }
+}
+public class ChildrenExtendsImplements {
+    public static void main(String[] args) {
+        A a = new C();
+        a.print();
+        B b =(B)a;
+        b.getNews();
+    }
+}
---
+++ Interface/Interface.java
@@ -0,0 +1,25 @@
+/*package Interface;
+interface IMessage{
+    public static final String MGS="i am a biter";  //全局常量
+    public abstract void print();   //抽象方法
+}
+
+interface INews{
+    public abstract String getNews(); //抽象方法
+}
+class MessageImpl implements IMessage,INews{
+         public void print(){
+             System.out.println("1"+IMessage.MGS);
+         }
+         public String getNews(){
+             return IMessage.MGS;
+         }
+        }
+public class Interface {
+    public static void main(String[] args) {
+        IMessage imessage = new MessageImpl();//向上转型实例化对象
+        imessage.print();  //调用被子类覆写过的方法
+        INews iNews = (INews) imessage;
+        System.out.println(iNews.getNews());
+    }
+}*/
