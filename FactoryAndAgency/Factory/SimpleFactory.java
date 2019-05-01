c92bba6e44c356882603293343f0834908786c0e
MMMMM104 <624244368@qq.com>
2019-05-01 18:32:09 +0800

    FactoryAndAgency

---
+++ FactoryAndAgency/Factory/SimpleFactory.java
@@ -0,0 +1,29 @@
+/*
+package FactoryAndAgency.Factory;
+
+import java.util.Scanner;
+
+interface Computer1{
+    public abstract  void printComputer();
+}
+class MacBookComputer implements Computer1{
+    public void printComputer(){
+        System.out.println("这是Mac");
+    }
+}
+class SurFaceComputer implements Computer1{
+    public void printComputer(){
+        System.out.println("这是Surface");
+    }
+}
+class FactoryComputer{
+
+}
+public class SimpleFactory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入你想要的电脑型号：(Mac/Sur)");
+        String answer= scanner.
+     }
+}
+*/
