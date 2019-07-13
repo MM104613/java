3e7d1cc00aa45bedb000777f6c49b8ea283d2c0d
MMMMM104 <624244368@qq.com>
2019-07-13 15:47:32 +0800

    cp

--- Thread/src/Thread/CPTest.java
+++ Thread/src/Thread/CPTest.java
@@ -1,9 +1,19 @@
 package Thread;
-class Goods{
+//生产者消费者模型
+import java.util.ArrayList;
+import java.util.List;
+
+class Goods {
     private String goodsName;
     private int count;
+    private int maxCount;
+
+    public Goods(int maxCount) {
+        this.maxCount = maxCount;
+    }
+
     public synchronized void set(String goodsName) {
-        if(count == 1){
+      while (count == maxCount) {
             System.out.println("还有商品，等待卖出");
             try {
                 this.wait();
@@ -14,11 +24,12 @@ class Goods{
         }
         this.goodsName = goodsName;
         count++;
-        System.out.println(Thread.currentThread().getName()+"生产"+this);
-        this.notify();//唤醒消费者
+        System.out.println(Thread.currentThread().getName() + "生产" + this);
+        this.notifyAll();//唤醒消费者
     }
-    public synchronized void get(){
-       if(count ==0){
+
+    public synchronized void get() {
+        while (count == 0) {
             System.out.println("没有商品，等待生产");
             try {
                 this.wait();
@@ -28,8 +39,8 @@ class Goods{
 
         }
         count--;
-        System.out.println(Thread.currentThread().getName()+"消费"+this);
-        this.notify();//唤醒生产者
+        System.out.println(Thread.currentThread().getName() + "消费" + this);
+        this.notifyAll();//唤醒生产者
     }
 
     @Override
@@ -40,19 +51,24 @@ class Goods{
                 '}';
     }
 }
-class Producer implements Runnable{
+
+class Producer implements Runnable {
     private Goods goods;
+
     public Producer(Goods goods) {
         this.goods = goods;
     }
 
     @Override
     public void run() {
-         this.goods.set("小黑瓶");
+       while(true){
+           this.goods.set("小黑瓶");
+       }
     }
 }
-class Consumer implements Runnable{
-    private  Goods goods;
+
+class Consumer implements Runnable {
+    private Goods goods;
 
     public Consumer(Goods goods) {
         this.goods = goods;
@@ -60,19 +76,27 @@ class Consumer implements Runnable{
 
     @Override
     public void run() {
-        this.goods.get();
+
+        while(true){
+            this.goods.get();
+        }
     }
 }
+
 public class CPTest {
     public static void main(String[] args) throws InterruptedException {
-        Goods goods = new Goods();
-        Producer producer =new Producer(goods);
-        Consumer consumer =new Consumer(goods);
-        Thread producerThread1 = new Thread(producer,"生产者1");
-        Thread consumerThread1 = new Thread(consumer,"消费者1");
-        consumerThread1.start();
-        Thread.sleep(1000);
-        producerThread1.start();
-
+        Goods goods = new Goods(10);
+        Producer producer = new Producer(goods);
+        Consumer consumer = new Consumer(goods);
+        List<Thread> list = new ArrayList<>();
+        //产生20个生产者
+        for (int i = 0; i < 20; i++)
+            list.add(new Thread(producer, "生产者" + (i + 1)));
+        //产生30个消费者
+        for (int i = 0; i < 30; i++)
+            list.add(new Thread(consumer, "消费者" + (i + 1)));
+         //启动所有生产、消费者
+        for (Thread thread :list)
+            thread.start();
     }
 }
