0c9fd1c257fe3571b05e00f946c8a1615ef61d32
MMMMM104 <624244368@qq.com>
2019-07-19 23:10:04 +0800

    Collection

---
+++ Collection/src/CollectionTest/CollectionTest.java
@@ -0,0 +1,21 @@
+package CollectionTest;
+/*JAVA类集里面（java.until包）提供了两个最为核心的接口：Collection、Map接口
+Collection接口：是JAVA中保存单个对象的最顶层接口
+常用的操作方法：
+增：public boolean add(E e)
+删：public boolean remove(Object o)
+查：public boolean contains(Object o)
+将集合变为对象数组返回：public Object[]toArray()
+用于集合输出：public Iterator<E> iterator()
+
+                  Collection
+使用率：80%      /           \
+(允许数据重复)list           set（不允许数据重复）
+            /     \
+          get     set
+*/
+public class CollectionTest {
+    public static void main(String[] args) {
+
+    }
+}
