13226fb9979dc61c0061780857c30eeb0682a352
MMMMM104 <624244368@qq.com>
2019-07-26 22:47:33 +0800

    Properties

---
+++ Collection/src/Properties/PropertiesTest.java
@@ -0,0 +1,11 @@
+package Properties;
+
+import java.io.File;
+import java.util.Properties;
+
+public class PropertiesTest {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        //File file = new File();
+    }
+}
