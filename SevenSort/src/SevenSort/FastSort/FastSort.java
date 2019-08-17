bada92950bcb8dc0b025bfc408843ce30ad25dd1
MMMMM104 <624244368@qq.com>
2019-08-17 22:37:44 +0800

    快排

--- SevenSort/src/SevenSort/FastSort/FastSort.java
+++ SevenSort/src/SevenSort/FastSort/FastSort.java
@@ -1,12 +1,16 @@
 package SevenSort.FastSort;
-
+//快速排序-单路快排
+/*
+思想： 6   4    9     3     2    5    8
+   values  i
+      j
+*/
 public class FastSort {
     public static void Fast(int[] arr) {
-        int n = arr.length;
-        if(n <= 1 ){
-            return;
-        }
-        quickSortInternal(arr,0,n-1);
+       int n = arr.length;
+       if(n <= 1)
+           return;
+       quickSortInternal(arr,0,n-1);
     }
     public static void quickSortInternal(int[] arr,int l,int r){
         if(r <= l)
@@ -15,23 +19,26 @@ public class FastSort {
         quickSortInternal(arr,l,q-1);
         quickSortInternal(arr,q+1,r);
     }
-    public static int partition(int[] arr,int l,int r){
+    public static int partition(int[] arr,int l, int r){
+        int randomNum = (int) (Math.random()*(r-l+1)+l);
+        Swap(arr,l,randomNum);
         int values = arr[l];
         int j = l;
-        int i =l+1;
+        int i = l+1;
         for(;i <= r;i++){
             if(arr[i] < values){
                 Swap(arr,i,j+1);
                 j++;
             }
         }
-        Swap(arr,l,j);
+        Swap(arr,j,l);
         return j;
     }
     public static void Swap(int[] arr,int a,int b){
         int temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
-    }
+     }
+
 
 }
