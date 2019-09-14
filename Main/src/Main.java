721d246546fa5642c24d2d4d3532ce8d35b54128
MMMMM104 <624244368@qq.com>
2019-09-14 19:43:55 +0800

    lian

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,32 +1,23 @@
-import java.util.ArrayList;
-import java.util.Collections;
+
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        ArrayList<Integer> w = new ArrayList<>();
-        if (n < 0 && n >100000 || w.size()<0 &&w.size() > 100000) {
-            System.out.println("出错了");
-        }
-        for (int i = 0; i < n; i++) {
-            w.add(s.nextInt());
-        }
-        int sum =0;
-        int f =0;
-        ArrayList<Integer> b = new ArrayList<>();
-        for(int j =1 ; j <= n ; j++){
-            for(int i = j-1; i < n ;i++){
-                sum +=w.get(i);
-            }
-            int min = Collections.min(w);
-            f = min * sum;
-            b.add(f);
-            b.remove(j-1);
-        }
-        int c= Collections.max(b);
-        System.out.println(c);
-    }
-}
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()){
+            char[] chars = scanner.next().toCharArray();
+            StringBuffer sb = new StringBuffer();
+            Set<Character> s = new HashSet<>();
+            for(int i =0;i <chars.length;i++ ){
+                if(s.add(chars[i])){
+                    sb.append(chars[i]);
+                }
 
+            }
+            System.out.println(sb.toString());
+        }
+    }
+
+}
